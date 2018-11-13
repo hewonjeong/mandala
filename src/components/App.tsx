130fac60b18b2742d78957b16795ba3bf5f36334
Hewon Jeong <hewon.jng@gmail.com>
2018-11-13 19:22:34 +0900

    feat: setup typescript

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div className="App">Let's make mandala art planner🕺🏻</div>;
+  }
+}
+
+export default App;
