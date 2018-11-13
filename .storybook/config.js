665cca10651d5f80226f47a9f2b58ee1a379f4dd
Hewon Jeong <hewon.jng@gmail.com>
2018-11-13 19:46:20 +0900

    feat: add stroybook

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+import { configure } from '@storybook/react';
+
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+function loadStories() {
+  requireAll(require.context('..', true, /.story.tsx?$/));
+}
+
+configure(loadStories, module);
